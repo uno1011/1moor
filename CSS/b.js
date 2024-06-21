6278427dd5914fe171635fcdd36013a12687b541
uno1011 <151750618+uno1011@users.noreply.github.com>
2024-06-21 18:06:05 +0800

    Update b.js

--- CSS/b.js
+++ CSS/b.js
@@ -1,1 +1,10 @@
+const audio = document.querySelector('audio');
+const body = document.querySelector('body');
 
+audio.addEventListener('play', () => {
+    body.classList.add('play-music');
+});
+
+audio.addEventListener('pause', () => {
+    body.classList.remove('play-music');
+});
